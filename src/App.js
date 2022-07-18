4ec052e9fa0d154f6bd9a37799319fe01dcd486c
darikmohammed <r.m.darik@gmail.com>
2022-07-18 23:57:28 +0300

    Add nav

--- src/App.js
+++ src/App.js
@@ -1,20 +1,20 @@
 import './App.css';
 import { Routes, Route } from 'react-router-dom';
-import Rockets from './routes/rockets';
-import Missions from './routes/missions';
+import Rockets from './routes/Rockets';
+import Missions from './routes/Missions';
 import MyProfile from './routes/MyProfile';
+import Nav from './Components/Nav/Nav';
 
 function App() {
   return (
-
     <div className="App">
+      <Nav />
       <h1>React App</h1>
       <Routes>
         <Route path="/" element={<Rockets />} />
         <Route path="/missions" element={<Missions />} />
         <Route path="/myprofile" element={<MyProfile />} />
       </Routes>
-
     </div>
   );
 }
