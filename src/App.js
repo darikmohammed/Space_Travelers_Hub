536896a114ad7afc71c6327a5b9599f78bc0dc9e
dqarias <dino.quispea@gmail.com>
2022-07-18 15:35:37 -0500

    Remove some files in src and Add routes folder

--- src/App.js
+++ src/App.js
@@ -1,9 +1,20 @@
 import './App.css';
+import { Routes, Route } from 'react-router-dom';
+import Rockets from './routes/rockets';
+import Missions from './routes/missions';
+import MyProfile from './routes/MyProfile';
 
 function App() {
   return (
+
     <div className="App">
       <h1>React App</h1>
+      <Routes>
+        <Route path="/" element={<Rockets />} />
+        <Route path="/missions" element={<Missions />} />
+        <Route path="/myprofile" element={<MyProfile />} />
+      </Routes>
+
     </div>
   );
 }
