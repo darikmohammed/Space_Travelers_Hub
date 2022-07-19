1c87e0dcaa35d1829db82762c651eb727a185697
darikmohammed <r.m.darik@gmail.com>
2022-07-19 22:27:00 +0300

    Update configureStore

--- src/Redux/configureStore.js
+++ src/Redux/configureStore.js
@@ -1,12 +1,12 @@
 import { configureStore } from '@reduxjs/toolkit';
 import { missionsReducer } from './Missions/missions';
+import fetchRockets from './Rockets/rocket';
 
-const store = configureStore(
-    {
-        reducer: {
-            missions : missionsReducer,
-        }
-    }
-);
+const store = configureStore({
+  reducer: {
+    missions: missionsReducer,
+    rockets: fetchRockets,
+  },
+});
 
 export default store;
