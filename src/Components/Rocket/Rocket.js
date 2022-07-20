d1fd26d8208444c081d9db9f3faef4a3c4b88531
darikmohammed <r.m.darik@gmail.com>
2022-07-21 01:34:35 +0300

    Add rocket reservation

--- src/Components/Rocket/Rocket.js
+++ src/Components/Rocket/Rocket.js
@@ -1,13 +1,13 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import './Rocket.css';
+import { useDispatch } from 'react-redux';
+import { updateRocketReservation } from '../../Redux/Rockets/rocket';
 
 function Rocket({
   name, description, reserved, image, id,
 }) {
-  const hundleReserve = (id) => {
-    console.log(id);
-  };
+  const dispatch = useDispatch();
   return (
     <div className="rocket">
       <img src={image} alt="name" />
@@ -19,7 +19,9 @@ function Rocket({
         </p>
         <button
           type="button"
-          onClick={hundleReserve(id)}
+          onClick={() => {
+            dispatch(updateRocketReservation(id));
+          }}
           className={reserved ? 'cancle-btn' : 'btn'}
         >
           {reserved ? 'Cancel Reservation' : 'Reserve Rocket'}
