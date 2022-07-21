3f3a01450d76c64c0d3b1de32d1575f400ff79c4
dqarias <dino.quispea@gmail.com>
2022-07-21 07:29:30 -0500

    Add styling for missions

--- src/Components/Missions/Mission.js
+++ src/Components/Missions/Mission.js
@@ -1,11 +1,18 @@
 import React from 'react';
 import PropTypes from 'prop-types';
+import './Mission.css';
 
 const Missions = ({ name, description }) => (
-  <div>
-    <p>{name}</p>
-    <p>{description}</p>
-  </div>
+
+  <li className="mission__table-row">
+    <div className="col col-1 table-name">{name}</div>
+    <div className="col col-2 table-description">{description}</div>
+    <div className="col col-3">
+      {' '}
+      <span className="badge badge-inactive">NOT A MEMBER</span>
+    </div>
+    <div className="col col-4"><button className="button button-primary" type="button">Join Mission</button></div>
+  </li>
 );
 
 export default Missions;
--- src/routes/Missions.js
+++ src/routes/Missions.js
@@ -1,20 +1,31 @@
 import React from 'react';
 import { useSelector } from 'react-redux';
 import Mission from '../Components/Missions/Mission';
+import './Missions.css';
 
 const Missions = () => {
   const missions = useSelector((state) => state.missions.missions);
 
   return (
-    <div>
-      {missions.map((mission) => (
-        <Mission
-          key={mission.mission_id}
-          id={mission.mission_id}
-          name={mission.mission_name}
-          description={mission.description}
-        />
-      ))}
+    <div className="mission__container">
+      <ul className="mission__table">
+        <li className="mission__table-header">
+          <div className="col col-1">Mission</div>
+          <div className="col col-2">Description</div>
+          <div className="col col-3">Status</div>
+          <div className="col col-4"> </div>
+        </li>
+
+        {missions.map((mission) => (
+          <Mission
+            key={mission.mission_id}
+            id={mission.mission_id}
+            name={mission.mission_name}
+            description={mission.description}
+          />
+        ))}
+
+      </ul>
     </div>
   );
 };
