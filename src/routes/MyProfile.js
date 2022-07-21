38288f1e5dd68826c395966446f67e713c6fbd16
darikmohammed <r.m.darik@gmail.com>
2022-07-21 22:35:05 +0300

    Add functionality for showing reserved rockets

--- src/routes/MyProfile.js
+++ src/routes/MyProfile.js
@@ -1,7 +1,40 @@
 import React from 'react';
+import { useSelector } from 'react-redux';
+import './MyProfile.css';
 
-const MyProfile = () => (
-  <div>MyProfile</div>
-);
+const MyProfile = () => {
+  const rockets = useSelector((state) => state.rockets.rockets);
+  const rocketsKey = Object.keys(rockets);
+  const reservedRocket = [];
+  rocketsKey.forEach((rocketId) => {
+    if (rockets[rocketId].reserved) {
+      reservedRocket.push({
+        id: rocketId,
+        name: rockets[rocketId].name,
+      });
+    }
+  });
+  return (
+    <div className="profile-display">
+      <div className="my-mission">
+        <h2>My Missions</h2>
+      </div>
+      <div className="my-rocket">
+        <h2>My Rockets</h2>
+        <div className="view-rockets">
+          {reservedRocket.length ? (
+            reservedRocket.map((rocket) => (
+              <tr key={rocket.id}>
+                <td>{rocket.name}</td>
+              </tr>
+            ))
+          ) : (
+            <p className="no-reserved">No Rockets Reserved</p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
 
 export default MyProfile;
