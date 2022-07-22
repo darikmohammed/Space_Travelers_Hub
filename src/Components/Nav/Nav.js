aa7415541f934f81c6703d702c5539df81147767
dqarias <dino.quispea@gmail.com>
2022-07-22 11:58:17 -0500

    Add functionality for responsive navbar

--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,29 +1,56 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { BiMenuAltLeft } from 'react-icons/bi';
 import logoImage from '../../img/planet.png';
 import './Nav.css';
 
-const Nav = () => (
-  <>
-    <nav className="nav-menus">
-      <NavLink to="/" className="logo-link">
-        <img src={logoImage} alt="logo" className="logo" />
-        <h2>Space Travelers&apos; Hub</h2>
-      </NavLink>
+const Nav = () => {
+  const [isNavExpanded, setIsNavExpanded] = useState(false);
 
-      <ul>
-        <NavLink to="/">
-          <li>Rockets</li>
+  return (
+    <>
+      <nav className="navigation">
+        <NavLink to="/" className="logo-link">
+          <img src={logoImage} alt="logo" className="logo" />
+          <h2>Space Travelers&apos; Hub</h2>
         </NavLink>
-        <NavLink to="/missions">
-          <li>Missions</li>
-        </NavLink>
-        <NavLink to="/myprofile">
-          <li>My Profile</li>
-        </NavLink>
-      </ul>
-    </nav>
-  </>
-);
+        <div className={isNavExpanded
+          ? 'navigation-menu expanded'
+          : 'navigation-menu'}
+        >
+          <ul>
+            <NavLink
+              to="/"
+              onClick={() => setIsNavExpanded(!isNavExpanded)}
+            >
+              <li>Rockets</li>
+            </NavLink>
+            <NavLink
+              to="/missions"
+              onClick={() => setIsNavExpanded(!isNavExpanded)}
+            >
+              <li>Missions</li>
+            </NavLink>
+            <NavLink
+              to="/myprofile"
+              onClick={() => setIsNavExpanded(!isNavExpanded)}
+            >
+              <li>My Profile</li>
+            </NavLink>
+          </ul>
+        </div>
+        <button
+          type="button"
+          className="hamburguer"
+          onClick={() => {
+            setIsNavExpanded(!isNavExpanded);
+          }}
+        >
+          <BiMenuAltLeft className="hamburguer-icon" />
+        </button>
+      </nav>
+    </>
+  );
+};
 
 export default Nav;
